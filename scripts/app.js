41de89b03348fcfc7b7c6357a487106816473e01
PedroRocha33 <rpedro1247@gmail.com>
2025-05-07 01:08:05 -0300

    Atualizacao

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,6 @@
 const nameCliente = document.querySelector('#cliente');
 const nameServico = document.querySelector('#tipoServico');
+const contatoCliente = document.querySelector('#contato')
 const dataServico = document.querySelector('#data');
 const valorServico = document.querySelector('#valor');
 const descService = document.querySelector('#descricao');
@@ -16,7 +17,8 @@ btnAdd.addEventListener('click', (event) => {
     let service = {
         nomeCliente: nameCliente.value,
         nomeServico: nameServico.value,
-        data: dataServico.value,
+        contato: contatoCliente.value,
+        data: dataServico.value.split('-').reverse().join('/'),
         valor: valorServico.value,
         descricao: descService.value,
         status: selectStatus.value,
@@ -30,11 +32,36 @@ btnAdd.addEventListener('click', (event) => {
     //Limpa os inputs
     nameCliente.value = '';
     nameServico.value = '';
+    contatoCliente.value = '';
     dataServico.value = '';
     valorServico.value = '';
     descService.value = '';
     selectStatus.value = '';
     prioridadeService.value = '';
 
+    let container = document.querySelector('.table-responsive');
+    let table = document.querySelector('#tabelaServicos');
+
+    listServices.forEach(item => {
+        table.innerHTML += `<tr>
+        <td>${item.nomeCliente}</td>
+        <td>${item.contato}</td>
+        <td>${item.nomeServico}</td>
+        <td>${item.descricao}</td>
+        <td>${item.data}</td>
+        <td>R$ ${item.valor}</td>
+        <td>${item.prioridade}</td>
+        <td><span class="status status-success">${item.status}</span></td>
+        <td>
+          <div class="table-actions">
+            <button class="action-btn" title="Visualizar"><i class="fas fa-eye"></i></button>
+            <button class="action-btn" title="Editar"><i class="fas fa-edit"></i></button>
+            <button class="action-btn" title="Excluir"><i class="fas fa-trash"></i></button>
+          </div>
+        </td>
+      </tr>`
+    })
+
+   
 });
 
